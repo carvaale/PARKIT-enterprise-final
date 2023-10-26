41e97a9d3b1a7e22194e9d05e0b1747684e6e5b9
Alex Carvalho <alexander_sck@live.ca>
2023-10-26 14:16:14 -0400

    feat: create listing DB context and map interface

---
+++ Repository/ParkingDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PARKIT_enterprise_final.Models;
+
+namespace PARKIT_enterprise_final.Repository
+{
+    public partial class ListingDBContext : DbContext
+    {
+        public ListingDBContext()
+        {
+        }
+
+        public ListingDBContext(DbContextOptions<ListingDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Listing> Listings { get; set; }
+    }
+}
