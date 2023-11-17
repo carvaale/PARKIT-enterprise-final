80311a8c98004cc3ee6b26ea2c6a3652252a5092
Frank Carusi <frankcarusi.fc@gmail.com>
2023-11-17 16:43:04 -0500

    Image model created, Listing and Seller updated, CreateListing [Get] created

---
+++ Models/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace PARKIT_enterprise_final.Models
+{
+    public class Image
+    {
+        public int ImgageId { get; set; }
+        public byte[] ImageData { get; set; }
+        public int ListingId { get; set; }
+    }
+}
