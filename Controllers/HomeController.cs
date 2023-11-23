1f65e9823b610b55e0091a65d5bd554f5f446c07
Alex Carvalho <alexander_sck@live.ca>
2023-11-23 14:15:20 -0500

    feat: add Listings interaction to map interface

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -66,6 +66,55 @@ namespace PARKIT_enterprise_final.Controllers
             return View();
         }
 
+        public IActionResult Map()
+        {
+
+            List<Listing> listings = new List<Listing>() 
+            { 
+                new Listing { 
+                    Address = new Address { 
+                        StreetAddress = "1234 Main St", 
+                        City = "Seattle", 
+                        ZipCode = "98101", 
+                        Latitude = "43.65804053299989", 
+                        Longitude = "-79.4349384105354"
+                    }, 
+                    IsAvailable = true, 
+                    StartTime = TimeSpan.Parse("10:00"), 
+                    EndTime = TimeSpan.Parse("12:00"), 
+                    Price = 10.00 
+                },
+                new Listing {
+                    Address = new Address {
+                        StreetAddress = "1234 Main St",
+                        City = "Seattle",
+                        ZipCode = "98101",
+                        Latitude = "43.64804053299989",
+                        Longitude = "-79.4349384105354 "
+                    },
+                    IsAvailable = true,
+                    StartTime = TimeSpan.Parse("10:00"),
+                    EndTime = TimeSpan.Parse("12:00"),
+                    Price = 10.00
+                },
+                new Listing {
+                    Address = new Address {
+                        StreetAddress = "1234 Main St",
+                        City = "Seattle",
+                        ZipCode = "98101",
+                        Latitude = "43.65804053299989",
+                        Longitude = "-79.4249384105354"
+                    },
+                    IsAvailable = true,
+                    StartTime = TimeSpan.Parse("10:00"),
+                    EndTime = TimeSpan.Parse("12:00"),
+                    Price = 10.00
+                },
+            };
+ 
+            return View(listings);
+        }
+
         public IActionResult Privacy()
         {
             return View();
