729b39170863281692696b708e1dcc204887bee1
Frank Carusi <frankcarusi.fc@gmail.com>
2023-11-23 12:35:51 -0500

    Changes made to reflect decisions made during meeting. DBContext changed, Listing properties added

--- Models/Listing.cs
+++ Models/Listing.cs
@@ -11,9 +11,12 @@ namespace PARKIT_enterprise_final.Models
         public Guid Id { get; set; }
         public Address Address { get; set; }
         public bool IsAvailable { get; set; }
+
+        public bool IsBooked { get; set; } = false;
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
 
+        //price per hour
         public double Price { get; set; }
 
         public List<Image>? Images { get; set; }
@@ -21,5 +24,9 @@ namespace PARKIT_enterprise_final.Models
         [FromForm(Name = "Images")]
         [NotMapped]
         public IFormFileCollection? Files { get; set; }
+
+        public Guid UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User? User { get; set; } // is only nullable until we figure out how to get the user id from the session
     }
 }
