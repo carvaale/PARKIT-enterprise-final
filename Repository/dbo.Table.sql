abcc0bae299d4398486d12d12a58c68ea8c334ec
Alex Carvalho <alexander_sck@live.ca>
2023-11-23 12:05:42 -0500

    refactor: db context

--- Repository/dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].TourInfo_Alexander
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Destination] VARCHAR(50) NOT NULL, 
-    [Guest] VARCHAR(50) NOT NULL, 
-    [Cost] FLOAT NOT NULL
-)
