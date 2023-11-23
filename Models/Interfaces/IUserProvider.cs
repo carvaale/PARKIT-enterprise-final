78efab9f541b2ee528aa115409227b2df8b80856
Ruiyan <shiruiyan0327@gmail.com>
2023-11-23 16:37:44 -0500

    Add user, address and wallet tables N operations

---
+++ Models/Interfaces/IUserProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace PARKIT_enterprise_final.Models.Interfaces
+{
+    public interface IUserProvider
+    {
+        User GetUser(int userId);
+        User AddUser(User user);
+        User UpdateUser(User user);
+    }
+}
---
+++ Models/Operations/AddressOperations.cs
@@ -0,0 +1,34 @@
+﻿using PARKIT_enterprise_final.Models.DBContext;
+using PARKIT_enterprise_final.Models.Interfaces;
+
+namespace PARKIT_enterprise_final.Models.Operations
+{
+    public class AddressOperations : IAddressOperations
+    {
+        private readonly PARKITDBContext _parkitDb;
+
+        public AddressOperations(PARKITDBContext parkitDb)
+        {
+            _parkitDb = parkitDb;
+        }
+        public Address AddAddress(Address address)
+        {
+            _parkitDb.Addresses.Add(address);
+            _parkitDb.SaveChanges();
+            return address;
+        }
+
+        public Address GetAddress(int userId)
+        {
+            return _parkitDb.Addresses.Find(userId);
+        }
+
+        public Address UpdateAddress(Address address)
+        {
+            var a = _parkitDb.Addresses.Attach(address);
+            a.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _parkitDb.SaveChanges();
+            return address;
+        }
+    }
+}
