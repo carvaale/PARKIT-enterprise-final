51a0b434eabe4a6254218b01eb0c1336f7c9829b
Alex Carvalho <alexander_sck@live.ca>
2023-11-23 12:03:51 -0500

    feat: add gmaps

---
+++ Repository/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].TourInfo_Alexander
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Destination] VARCHAR(50) NOT NULL, 
+    [Guest] VARCHAR(50) NOT NULL, 
+    [Cost] FLOAT NOT NULL
+)
