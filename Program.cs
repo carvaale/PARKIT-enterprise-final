925db5de07f6b54fb16f15411c25739e00e2a6c7
Ruiyan <shiruiyan0327@gmail.com>
2023-11-26 21:10:08 -0500

    set controller route back to default

--- Program.cs
+++ Program.cs
@@ -49,6 +49,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=User}/{action=login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
