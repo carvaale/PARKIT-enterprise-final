eb2cf098f985a0016cf1ce30e803584074370c70
Alex Carvalho <alexander_sck@live.ca>
2023-12-02 18:03:07 -0500

    fix: default page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PARKIT_enterprise_final.Models;
 using PARKIT_enterprise_final.Models.Interfaces;
-using PARKIT_enterprise_final.ViewModels;
 using System.Diagnostics;
 
 namespace PARKIT_enterprise_final.Controllers
@@ -9,42 +8,15 @@ namespace PARKIT_enterprise_final.Controllers
     public class HomeController : Controller
     {
         private readonly IMapPointProvider _mapPointProvider;
-        private readonly IListingsProvider _listingProvider;
-        private readonly ILogger<HomeController> _logger;
-        private readonly IBookingProvider _bookingProvider;
-        private readonly IUserProvider _userProvider;
-        private readonly IWalletProvider _walletProvider;
-        private readonly IHttpContextAccessor _contextAccessor;
 
-        public HomeController(
-            ILogger<HomeController> logger, 
-            IListingsProvider listingsProvider,
-            IBookingProvider bookingProvider,
-            IUserProvider userProvider,
-            IWalletProvider walletProvider,
-            IMapPointProvider mapPointProvider,
-            IHttpContextAccessor contextAccessor)
+
+        public HomeController(IMapPointProvider mapPointProvider)
         {
-            _logger = logger;
-            _listingProvider = listingsProvider;
-            _bookingProvider = bookingProvider;
-            _userProvider = userProvider;
-            _walletProvider = walletProvider;
             _mapPointProvider = mapPointProvider;
-            _contextAccessor = contextAccessor;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            if(_contextAccessor.HttpContext.Session.GetString("CurrentUser") != null)
-            {
-                return RedirectToAction("Account", "Home");
-            }
-            return RedirectToAction("CreateUser", "User");
-        }
-
-        public IActionResult Map()
-        {            
             List<MapPoint> mapPoints = _mapPointProvider.GetMapPoints();
             ViewData["GMAP_API_KEY"] = Environment.GetEnvironmentVariable("GOOGLE_API_KEY");
 
