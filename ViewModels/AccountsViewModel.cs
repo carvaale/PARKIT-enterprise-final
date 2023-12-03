b7a02d9b3ab3b72999bd2b13161c454750a1ed90
Syed Taha Faisal <faisasye@sheridancollege.ca>
2023-12-03 04:02:37 -0500

    Added comments, cleaned up code.

--- ViewModels/AccountsViewModel.cs
+++ ViewModels/AccountsViewModel.cs
@@ -2,10 +2,14 @@
 
 namespace PARKIT_enterprise_final.ViewModels
 {
+    /// <summary>
+    /// Author: Syed Taha Faisal
+    /// Description: ViewModel for representing the data required for account-related views.
+    /// This includes details about the user, their bookings, and their wallet.
+    /// </summary>
     public class AccountsViewModel
     {
         public User User { get; set; }
-/*        public Booking Booking { get; set; }*/
         public List<Booking> BookingList { get; set; }
         public Wallet Wallet { get; set; }
     }
