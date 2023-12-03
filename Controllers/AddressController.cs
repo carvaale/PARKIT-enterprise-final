b96d945addc6b36d717e2c0c7f164c0e6dec4d50
Ruiyan <shiruiyan0327@gmail.com>
2023-12-03 00:11:43 -0500

    add all comments

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿/// <summary>
+/// Created by Ruiyan Shi 
+/// standard controller for the Address object, used for control the traffic of Address
+/// </summary>
+using Microsoft.AspNetCore.Mvc;
 using PARKIT_enterprise_final.Models.Interfaces;
 using PARKIT_enterprise_final.Models;
 
@@ -6,17 +10,20 @@ namespace PARKIT_enterprise_final.Controllers
 {
     public class AddressController : Controller
     {
+        // interface to access address provider
         private readonly IAddressProvider _addressProvider;
         public AddressController(IAddressProvider addressProvider)
         {
             _addressProvider = addressProvider;
         }
 
+        // default index controller
         public IActionResult Index()
         {
             return View();
         }
 
+        // get and post function to edit address
         [HttpGet]
         public IActionResult Edit(Guid id)
         {
@@ -32,6 +39,7 @@ namespace PARKIT_enterprise_final.Controllers
             return RedirectToAction("AllAddresses");
         }
 
+        // get all addresses and return the list to view
         public IActionResult AllAddresses()
         {
             return View(_addressProvider.GetAddresses());
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿/// <summary>
+/// Created by Ruiyan Shi 
+/// controller for the User object, used for control the traffic of User
+/// </summary>
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using PARKIT_enterprise_final.Models;
@@ -16,24 +20,35 @@ namespace PARKIT_enterprise_final.Controllers
             _userProvider = userProvider;
             _contextAccessor = httpContextAccessor;
         }
+
+        // default user index
         public IActionResult Index()
         {
             return View();
         }
 
+        // controller for login
+        // get login returns the login view
         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        // post login will accept users input of username and password
+        // then check the password with database
+        // if the password match, will log the uer in
+        // if not, return the error message
         [HttpPost]
         public IActionResult Login(User user)
         {
             string userName = user.Username;
+            // get the user from data base by username
             User userInDatabase = _userProvider.GetUserByUsername(userName);
+            //  if the database returns an existing user or null
             if (userInDatabase != null)
             {
+                // if the password matches
                 if (userInDatabase.Password == user.Password)
                 {
                     // login successfully
@@ -44,46 +59,58 @@ namespace PARKIT_enterprise_final.Controllers
                     return RedirectToAction("Account", "Account");
                 }
             }
+            // add the error message and pass back to view by viewbag
             ViewBag.Error = "UserName and Password doesn't match, please try again";
             return View();
         }
 
+        // get all users
         [HttpGet]
         public IActionResult AllUsers()
         {
             return View(_userProvider.GetUsers());
         }
 
+        // get function to get the page for creating new user
         [HttpGet]
         public IActionResult CreateUser()
         {
             return View();
         }
 
+        // post request to create new user
         [HttpPost]
         public IActionResult CreateUser(User user)
         {
+            // if the user input is valid
             if (ModelState.IsValid)
             {
+                // check if the username is used already
                 string userName = user.Username;
                 User userInDatabase = _userProvider.GetUserByUsername(userName);
+                // if returns null mean not used
                 if(userInDatabase == null)
                 {
+                    // create the user
                     User u = _userProvider.CreateUser(user);
+                    // set the wallet ID and update it
                     u.WalletId = u.Wallet.WalletId;
                     _userProvider.UpdateUser(u);
 
+                    // add the login info to the session after register successfully
                     var session = _contextAccessor.HttpContext.Session;
                     session.SetString("CurrentUserName", u.FirstName.ToString());
                     session.SetString("CurrentUser", JsonConvert.SerializeObject(u));
                     // need to change the destination
                     return RedirectToAction("Account", "Account");
                 }
+                // return to the view with the error message
                 ViewBag.Error = "UserName already exists, please try another one!";
             }
             return View();
         }
 
+        // delete the user
         [HttpGet]
         public IActionResult DeleteUser(Guid id)
         {
@@ -97,6 +124,7 @@ namespace PARKIT_enterprise_final.Controllers
             return RedirectToAction("AllUsers");
         }
 
+        // check user details
         [HttpGet]
         public IActionResult UserDetails(Guid id)
         {
@@ -105,12 +133,15 @@ namespace PARKIT_enterprise_final.Controllers
             return View(user);
         }
 
+        // update user, using id to get existing user info
         [HttpGet]
         public IActionResult UpdateUser(Guid id)
         {
             User user = _userProvider.GetUser(id);
             return View(user);
         }
+
+        // post request to update the user in the database
         [HttpPost]
         public IActionResult UpdateUser(User user)
         {
@@ -119,6 +150,7 @@ namespace PARKIT_enterprise_final.Controllers
             return RedirectToAction("AllUsers");
         }
 
+        // logout the user by clear the session
         [HttpGet]
         public IActionResult LogoutUser()
         {
